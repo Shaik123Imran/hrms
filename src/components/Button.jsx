13a4cdce115cf889f4d0c673f8eeb9741d1f6846
Shaik123Imran <149596572+Shaik123Imran@users.noreply.github.com>
2026-09-16 11:03:37 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,1 @@
-export default function Button({ variant = 'primary', size, type = 'button', className = '', children, ...props }) {
-  const classNames = ['btn', `btn-${variant}`, size === 'sm' ? 'btn-sm' : '', className]
-    .filter(Boolean)
-    .join(' ');
 
-  return (
-    <button type={type} className={classNames} {...props}>
-      {children}
-    </button>
-  );
-}
