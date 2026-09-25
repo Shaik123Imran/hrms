8475af100944bab9cb0e66405b1a39570b4d19ee
Rentala Lakshmi Sree <224g1a3341@srit.ac.in>
2026-09-25 10:26:26 +0530

    Update button and employee form

--- src/pages/EmployeeForm/EmployeeForm.jsx
+++ src/pages/EmployeeForm/EmployeeForm.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import data from "../../data/data.json";
 import "./EmployeeForm.css";
-import Button from "../../components/Button";
+import Button from "../../components/ui/Button";
 import { ArrowLeft } from "lucide-react";
 import { validateEmployee } from "./employeeValidation";
 
